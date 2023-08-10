4c05f050db51dbf66b496bc427b2a72270771dea
Lucas Azevedo <111284802+LucasBumbstead@users.noreply.github.com>
2023-08-10 16:21:44 -0300

    vesion2

--- src/App.js
+++ src/App.js
@@ -1,83 +1,144 @@
-import React, { useEffect, useState } from 'react';
-import './App.css' 
-import tmdb from './tmdb';
-import MovieRow from './components/movieRow';
-import FeaturedMovie from './components/featuredmovie';
-import Header from './components/header';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Movie from "./components/Movie";
+import Youtube from "react-youtube";
 
-export default () => {
+function App() {
+  const MOVIE_API = "https://api.themoviedb.org/3/";
+  const SEARCH_API = MOVIE_API + "search/movie";
+  const DISCOVER_API = MOVIE_API + "discover/movie";
+  const API_KEY = "fca20a8a62aeb9dec3aca16c54e9a58f";
+  const BACKDROP_PATH = "https://image.tmdb.org/t/p/w1280";
 
+  const [playing, setPlaying] = useState(false);
+  const [trailer, setTrailer] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
 
-  const [movieList, setMovieList] = useState([]);
-  const [featuredData, setFeaturedData] = useState(null);
-  const [blackHeader, setBlackHeader] = useState(false);
-
-
-  useEffect(()=>{
-    const loadAll = async () => {
-      //Pegando lista TOTAL
-      let list = await tmdb.getHomeList();
-      setMovieList(list);
-
-      // pegando o featured
-      let originals = list.filter(i=>i.slug == 'originals');
-      let ramdomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
-      let chosen = originals[0].items.results[ramdomChosen];
-      let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
-      setFeaturedData(chosenInfo);
-
-    }
-
-
-    loadAll();
-
+  useEffect(() => {
+    fetchMovies();
   }, []);
 
-  useEffect(()=>{
-    const scrollListener = () => {
-      if(window.scrollY > 10){
-        setBlackHeader(true);
-      } else{
-        setBlackHeader(false);
-      }
+    const fetchMovies = async (event) => {
+        if (event) {
+            event.preventDefault()
+        }
+
+        const {data} = await axios.get(`${searchKey ? SEARCH_API : DISCOVER_API}`, {
+            params: {
+                api_key: API_KEY,
+                query: searchKey
+            }
+        })
+
+        console.log(data.results[0])
+        setMovies(data.results)
+        setMovie(data.results[0])
+
+        if (data.results.length) {
+            await fetchMovie(data.results[0].id)
+        }
     }
 
-    window.addEventListener('scroll', scrollListener);
-    return() =>{
-      window.removeEventListener('scroll', scrollListener);
+    const fetchMovie = async (id) => {
+        const {data} = await axios.get(`${MOVIE_API}movie/${id}`, {
+            params: {
+                api_key: API_KEY,
+                append_to_response: "videos"
+            }
+        })
+
+        if (data.videos && data.videos.results) {
+            const trailer = data.videos.results.find(vid => vid.name === "Official Trailer")
+            setTrailer(trailer ? trailer : data.videos.results[0])
+        }
+
+        setMovie(data)
     }
 
-  }, []);
 
-  return (
-    <div className="page">
+    const selectMovie = (movie) => {
+        fetchMovie(movie.id)
+        setPlaying(false)
+        setMovie(movie)
+        window.scrollTo(0, 0)
+    }
 
-      <Header black={blackHeader} />
+    const renderMovies = () => (
+        movies.map(movie => (
+            <Movie
+                selectMovie={selectMovie}
+                key={movie.id}
+                movie={movie}
+            />
+        ))
+    )
 
-      {featuredData && 
-        <FeaturedMovie item={featuredData} />
-      }
+    return (
+        <div className="App">
+            <header className="center-max-size header">
+                <img className="logo" src="./assets/logo.png" alt="Logo Jaca Flix!"  />
+                <form className="form" onSubmit={fetchMovies}>
+                    <input className="search" type="text" id="search"
+                           onInput={(event) => setSearchKey(event.target.value)}/>
+                    <button className="submit-search" type="submit"><i className="fa fa-search"></i></button>
+                </form>
+            </header>
+            {movies.length ?
+                <main>
+                    {movie ?
+                        <div className="poster"
+                             style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${BACKDROP_PATH}${movie.backdrop_path})`}}>
+                            {playing ?
+                                <>
+                                    <Youtube
+                                        videoId={trailer.key}
+                                        className={"youtube amru"}
+                                        containerClassName={"youtube-container amru"}
+                                        opts={
+                                            {
+                                                width: '100%',
+                                                height: '100%',
+                                                playerVars: {
+                                                    autoplay: 1,
+                                                    controls: 0,
+                                                    cc_load_policy: 0,
+                                                    fs: 0,
+                                                    iv_load_policy: 0,
+                                                    modestbranding: 0,
+                                                    rel: 0,
+                                                    showinfo: 0,
+                                                },
+                                            }
+                                        }
+                                    />
+                                    <button onClick={() => setPlaying(false)} className={"button close-video"}>Close
+                                    </button>
+                                </> :
+                                <div className="center-max-size">
+                                    <div className="poster-content">
+                                        {trailer ?
+                                            <button className={"button play-video"} onClick={() => setPlaying(true)}
+                                                    type="button">Play
+                                                Trailer</button>
+                                            : 'Sorry, no trailer available'}
+                                        <h1>{movie.title}</h1>
+                                        <p>{movie.overview}</p>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                        : null}
 
-      
-
-      <section className="lists">
-        {movieList.map((item,key)=>(
-          <MovieRow key={key} title= {item.title} items={item.items} />
-
-        ))}
-      </section> 
-
-      <footer>
-        Feito com <span role ="img" aria-label="coração">❤️</span> pelo Grupo4 de ES aplicada<br/>
-        Direitos de imagem para Netflix<br/>
-        Dados pegos do site Themoviedb.org
-
-      </footer>
-      {movieList.length <= 0 &&
-      <div className="loading">
-        <img src = "https://media0.giphy.com/media/3y0oCOkdKKRi0/giphy.gif?cid=ecf05e473dd2rxbip6djqlmk38lgzcn4puttok8qpnl3hd0u&ep=v1_gifs_search&rid=giphy.gif&ct=g"></img>
-      </div>
-      }
-    </div>
-  );
+                    <div className={"center-max-size container"}>
+                        {renderMovies()}
+                    </div>
+                </main>
+                : 'Sorry, no movies found'}
+        </div>
+    );
 }
+
+export default App;
