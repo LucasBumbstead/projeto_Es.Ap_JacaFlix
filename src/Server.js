c21c51831c6d27203f80c1915ab3fb10f292e3db
DaviIcaro <daviicaroytb@gmail.com>
2023-11-07 19:02:05 -0300

    remodulando o server js

---
+++ src/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Servir arquivos estáticos a partir da pasta de compilação
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Configurar uma rota para lidar com todas as solicitações
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor está ouvindo na porta ${port}`);
+});
