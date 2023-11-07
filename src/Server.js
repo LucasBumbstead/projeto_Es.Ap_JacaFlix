c8d05d7575f66075add118565897a266b5b05fb1
DaviIcaro <daviicaroytb@gmail.com>
2023-11-07 19:37:11 -0300

    ajustando o server js

--- src/Server.js
+++ src/Server.js
@@ -4,13 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 // Servir arquivos estáticos a partir da pasta de compilação
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Configurar uma rota para lidar com todas as solicitações
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Servidor está ouvindo na porta ${port}`);
