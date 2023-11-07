14181b5fd67208b78bc7aed8d129a06532329f23
Davi Ícaro <108202235+DaviIcaro@users.noreply.github.com>
2023-11-07 20:42:44 -0300

    edit Server.js

--- src/Server.js
+++ src/Server.js
@@ -1,53 +1,22 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    // Serve o arquivo HTML que inclui seu aplicativo React
-    const filePath = path.join(__dirname, 'public', 'index.html');
+app.use(express.json());
 
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Erro interno do servidor');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
+app.get('/api/movies', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/your-endpoint', {
+      params: req.query,
     });
-  } else {
-    // Lidar com outras solicitações, como recursos estáticos (CSS, JavaScript)
-    const staticFilePath = path.join(__dirname, 'public', req.url);
-    
-    fs.readFile(staticFilePath, (err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.end('Recurso não encontrado');
-      } else {
-        // Determinar o tipo de conteúdo com base na extensão do arquivo
-        const contentType = getContentType(req.url);
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(data);
-      }
-    });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error');
   }
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${PORT}`);
 });
-
-// Função auxiliar para determinar o tipo de conteúdo com base na extensão do arquivo
-function getContentType(filename) {
-  const extname = path.extname(filename);
-  switch (extname) {
-    case '.js':
-      return 'text/javascript';
-    case '.css':
-      return 'text/css';
-    default:
-      return 'text/plain';
-  }
-}
