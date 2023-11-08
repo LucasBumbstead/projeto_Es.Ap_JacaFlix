ef6ce9aa3a8271873d2911bd627d4dc63f183aef
Davi Ícaro <108202235+DaviIcaro@users.noreply.github.com>
2023-11-07 22:11:50 -0300

    Update Server.js

--- src/Server.js
+++ src/Server.js
@@ -6,12 +6,10 @@ const PORT = 3000;
 console.log('__dirname:', __dirname);
 
 // Defina a pasta "public" como pasta de arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Rota para servir a página principal
 app.get('/', (req, res) => {
-  console.log('Serving index.html from:', path.join(__dirname, 'public', 'index.html'));
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
